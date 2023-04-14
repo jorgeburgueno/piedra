5dbf5277b546303e442c9d6df5063baf7e070e0e
Jorge Burgueno <jorge.albert16@hotmail.com>
2023-04-13 20:42:01 -0700

    creado la parte del juego

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-function getComputerChoise(uno, dos, tres) {
+const opcionesDeJuego= ['Piedra', 'Papel', 'Tijera']
+
+function getComputerChoise() {
      const opcionesDeJuego= ['Piedra', 'Papel', 'Tijera']
-     const opcionRandom= Math.floor(Math.random() * opcionesDeJuego.length);
-     console.log(opcionesDeJuego[opcionRandom]) 
-   
+     return opcionRandom= opcionesDeJuego[Math.floor (Math.random() * opcionesDeJuego.length)];
 }
-getComputerChoise()
+
+function eleccionDelJugador() {
+  return prompt('Piedra,papel o tijera?');
+}
+
+
+function juego () {
+
+    if (seleccionDelJugador.toLowerCase() == "tijera" && seleccionDeMaquina == "Piedra") {
+        alert ("Pierdes!, Piedra gana a Tijera");}
+    else if (seleccionDelJugador.toLowerCase() == "piedra" && seleccionDeMaquina == "Piedra") {
+      alert ("Empate!");}    
+    else if (seleccionDelJugador.toLowerCase() == "papel" && seleccionDeMaquina == "Piedra") {
+      alert ("Tu ganas!, Papel gana a Piedra!");}  
+    else if (seleccionDelJugador.toLowerCase() == "tijera" && seleccionDeMaquina == "Papel") {
+      alert ("Tu ganas!, Tijera gana a Papel!");}     
+    else if (seleccionDelJugador.toLowerCase() == "piedra" && seleccionDeMaquina == "Papel") {
+      alert ("Pierdes!,Papel gana a Piedra");}     
+    else if (seleccionDelJugador.toLowerCase() == "papel" && seleccionDeMaquina == "Papel") {
+      alert ("Empate!");} 
+    else if (seleccionDelJugador.toLowerCase() == "tijera" && seleccionDeMaquina == "Tijera") {
+      alert ("Empate");} 
+    else if (seleccionDelJugador.toLowerCase() == "piedra" && seleccionDeMaquina == "Tijera") {
+      alert ("Tu ganas!, Piedra gana a Tijeras");} 
+    else if (seleccionDelJugador.toLowerCase() == "papel" && seleccionDeMaquina == "Tijera") {
+      alert ("Pierdes!, Tijera gana a Papel");} 
+    
+}
+  
+   
+
+
+
+const seleccionDelJugador = eleccionDelJugador();
+const seleccionDeMaquina = getComputerChoise();
+console.log(juego(seleccionDelJugador, seleccionDeMaquina));
